c1326c077ec35f0a9414af2b75cea095147defe2
Đức Hạnh Nguyễn <nguyenhanhthucquy@gmail.com>
2022-07-18 10:55:56 +0300

    Add one cat to index

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var Dog = require('./Dog');
 var Cat = require('./Cat');
 
 var dog = new Dog('Tom');
+var dog = new Dog('Tho');
+
 var tom = new Cat();
+var tho = new Cat();
+
 dog.eat(tom);
+dog.eat(tho);
 console.log(dog);
